823ada5dcaba432f6af453ae7665a8b78fc2e785
USER <USER@USER-PC>
2018-01-25 17:46:33 +0900

    c

---
+++ groupwareWebApp/src/main/java/com/bit/groupware/service/authority/BoardServiceImpl.java
@@ -0,0 +1,29 @@
+package com.bit.groupware.service.authority;
+
+import java.util.List;
+
+import com.bit.groupware.domain.authority.BoardVO;
+
+public class BoardServiceImpl implements BoardService {
+
+	public List<BoardVO> retrieveBoard(List<BoardVO> boards) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void registerBoard(BoardVO board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void modifyBoard(BoardVO article) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
