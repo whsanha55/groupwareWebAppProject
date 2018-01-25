8832bd00f3c675af8653da3f4d8fcef88904629d
USER <USER@USER-PC>
2018-01-25 13:20:14 +0900

    c

--- groupwareWebApp/src/main/java/com/bit/groupware/domain/authority/RoleVO.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/domain/authority/RoleVO.java
@@ -7,49 +7,72 @@ public class RoleVO {
 	private String rExplan; 		//롤 설명
 	private String rDate;			 // 등록 일자 
 	private String isRegistration; //등록 여부
+	private int sortOrder; 			//정렬 순서
 	
 	public RoleVO() {
 		super();
 	}
+
 	public String getrId() {
 		return rId;
 	}
+
 	public void setrId(String rId) {
 		this.rId = rId;
 	}
+
 	public String getrName() {
 		return rName;
 	}
+
 	public void setrName(String rName) {
 		this.rName = rName;
 	}
+
 	public String getrType() {
 		return rType;
 	}
+
 	public void setrType(String rType) {
 		this.rType = rType;
 	}
+
 	public String getrExplan() {
 		return rExplan;
 	}
+
 	public void setrExplan(String rExplan) {
 		this.rExplan = rExplan;
 	}
+
 	public String getrDate() {
 		return rDate;
 	}
+
 	public void setrDate(String rDate) {
 		this.rDate = rDate;
 	}
+
 	public String getIsRegistration() {
 		return isRegistration;
 	}
+
 	public void setIsRegistration(String isRegistration) {
 		this.isRegistration = isRegistration;
 	}
+
+	public int getSortOrder() {
+		return sortOrder;
+	}
+
+	public void setSortOrder(int sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
 	@Override
 	public String toString() {
 		return "RoleVO [rId=" + rId + ", rName=" + rName + ", rType=" + rType + ", rExplan=" + rExplan + ", rDate="
-				+ rDate + ", isRegistration=" + isRegistration + "]";
+				+ rDate + ", isRegistration=" + isRegistration + ", sortOrder=" + sortOrder + "]";
 	}
+	
 }
