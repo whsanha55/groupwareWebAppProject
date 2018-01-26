b84dfda7373ada27fc71fa8ea44f7ac7ba8a4e13
USER <USER@USER-PC>
2018-01-26 11:37:24 +0900

    modify - planFileDAOImpl

--- groupwareWebApp/src/main/java/com/bit/groupware/persistent/employee/PlanFileDAOImpl.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/persistent/employee/PlanFileDAOImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.bit.groupware.domain.employee.PlanFileVO;
 
 @Repository
-public class PlanFileDAOImpl {
+public class PlanFileDAOImpl implements PlanFileDAO {
 	private static final String NAMESPACE = "com.bit.groupware.persistent.mapper.employee.PlanFileMapper";
 	
 	@Autowired
