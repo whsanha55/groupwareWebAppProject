65bd2062821181703a787fefcfee7ff0f2c22c28
Jihee-Lee92 <dhkdnun@naver.com>
2018-01-26 11:10:52 +0900

    jh

--- groupwareWebApp/src/main/java/com/bit/groupware/service/approval/TemplateBookmarkServiceImpl.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/service/approval/TemplateBookmarkServiceImpl.java
@@ -1,16 +1,22 @@
 package com.bit.groupware.service.approval;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.bit.groupware.domain.approval.TemplateBookmarkVO;
-
+import com.bit.groupware.persistent.approval.TemplateBookmarkDAO;
+@Service
 public class TemplateBookmarkServiceImpl implements TemplateBookmarkService {
-
+	
+	@Autowired
+	private TemplateBookmarkDAO templateBookmark;
+	
 	public void registerTemplateBookmark(TemplateBookmarkVO templateBookmarkVO) {
-		// TODO Auto-generated method stub
-
+		templateBookmark.insertTemplateBookmark(templateBookmarkVO); 
 	}
 
 	public void removeTemplateBookmark(int bookmarkNo) {
-		// TODO Auto-generated method stub
+		templateBookmark.deleteTemplateBookmark(bookmarkNo);
 
 	}
 
