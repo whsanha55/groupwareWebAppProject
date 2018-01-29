1aeff4f0c781705cee2439c61869b7424c55d8c4
USER <USER@USER-PC>
2018-01-29 14:58:51 +0900

    ccccc

--- groupwareWebApp/src/main/java/com/bit/groupware/controller/LoginController.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/controller/LoginController.java
@@ -1,18 +1,30 @@
 package com.bit.groupware.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@SessionAttributes("emp")
 @Controller
 public class LoginController {
 
+	//Logging
+	public static final Logger Logger = LoggerFactory.getLogger(LoginController.class);
+	
 	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
-	public String form() {
+	public String form(@RequestParam(value="id", required=true) String id) {
 		return "login";
 	}
 	
+	
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public String submit() {
+		return "login";
+	}
 
 
 	
