8c7320d8a9138d7bf55f6e96f1adb9f2b007aecd
USER <USER@USER-PC>
2018-01-30 09:48:35 +0900

    c

--- groupwareWebApp/src/main/java/com/bit/groupware/controller/authority/PasswordEncoderController.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/controller/authority/PasswordEncoderController.java
@@ -3,7 +3,7 @@ package com.bit.groupware.controller.authority;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PasswordEncoderController {
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private BCryptPasswordEncoder bcryptPasswordEncoder;
 	private final static Logger logger = LoggerFactory.getLogger(PasswordEncoderController.class);
 	
 	
 	@RequestMapping(value="/passwordEncode.do", method=RequestMethod.GET)
 	public String  passwordEncode() {
 		String rawPassword = "1234";
-		String encodedPassword = passwordEncoder.encode(rawPassword);
+		String encodedPassword = bcryptPasswordEncoder.encode(rawPassword);
 		logger.info("rawPassword : " + rawPassword);
 		logger.info("encodedPassword : " + encodedPassword);
 		return "success";
