d0bc9db7cf55b1e1ec90a469870241c8f1f6ccd7
USER <USER@USER-PC>
2018-01-30 20:09:01 +0900

    commit

--- groupwareWebApp/src/main/java/com/bit/groupware/persistent/authority/BoardDAOImpl.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/persistent/authority/BoardDAOImpl.java
@@ -2,28 +2,36 @@ package com.bit.groupware.persistent.authority;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.bit.groupware.domain.authority.BoardVO;
 
+@Repository
 public class BoardDAOImpl implements BoardDAO {
 
+	private static final String NAMESPACE = "com.bit.groupware.persistent.mapper.authority.BoardMapper";
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
 	public List<BoardVO> selectBoard() {
-		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectList(NAMESPACE + ".selectBoardList");
 	}
 
 	public void insertBoard(BoardVO board) {
-		// TODO Auto-generated method stub
-		
+		sqlSession.insert(NAMESPACE + ".insertBoard" , board);
 	}
 
 	public void updateBoard(BoardVO board) {
-		// TODO Auto-generated method stub
+		sqlSession.update(NAMESPACE + ".updateBoard", board);
 		
 	}
 
 	public void deleteBoard(int boardNo) {
-		// TODO Auto-generated method stub
-		
+		sqlSession.delete(NAMESPACE + ".deleteBoard" , boardNo);
 	}
 
 }
