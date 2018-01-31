524ce415be2c29aa4e3c5d7f82695a4d4981e7db
USER <USER@USER-PC>
2018-01-31 20:12:37 +0900

    commit;

--- groupwareWebApp/src/main/java/com/bit/groupware/controller/authority/AdminRegisterAuthorityController.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/controller/authority/AdminRegisterAuthorityController.java
@@ -1,27 +1,19 @@
 package com.bit.groupware.controller.authority;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.bit.groupware.service.authority.BoardService;
 
 @Controller
 public class AdminRegisterAuthorityController {
 
-	@Autowired
-	private BoardService boardService;
+
 
 	@RequestMapping(value = "/admin/authority.do", method = RequestMethod.GET)
-	public ModelAndView form() {
-		ModelAndView mv = new ModelAndView();
+	public String form() {
+		return "authority/admin_authority";
 
-		mv.addObject("boards", boardService.retrieveBoard());
-		mv.setViewName("authority/admin_authority");
-
-		return mv;
 	}
 
 }
