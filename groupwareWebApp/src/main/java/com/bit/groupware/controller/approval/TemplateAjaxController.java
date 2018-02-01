f2157305966e0d25c115c69741d5ebe6339caf98
USER <USER@USER-PC>
2018-02-01 20:19:24 +0900

    tmp tmp

--- groupwareWebApp/src/main/java/com/bit/groupware/controller/approval/TemplateAjaxController.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/controller/approval/TemplateAjaxController.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,32 +14,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bit.groupware.domain.approval.TemplateVO;
-import com.bit.groupware.domain.authority.UserVO;
 import com.bit.groupware.service.approval.TemplateService;
 
 @Controller
 public class TemplateAjaxController {
-
+	
 	@Autowired
 	private TemplateService templateService;
 	
 	@RequestMapping(value = "/templatePagingAjax.do", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String,Object> getTemplateList(
-			@RequestParam String keyfield ,
+			@RequestParam(required=false) String keyfield,
 			@RequestParam(required=false) String keyword ,
 			@RequestParam int startRow ,
 			@RequestParam int endRow
 			) {
 		Map<String, Object> map = new HashMap<String, Object>();
-		
+
 //		UserVO user = (UserVO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 //		map.put("empNo", user.getUsername());
-		map.put("empNo", "2018-00011");
+		map.put("empNo", "2018-00011");		
 		
 		map.put("keyfield", keyfield);
 		map.put("keyword", keyword);
-	
+			
 		int totalCount = templateService.retrieveTemplateCount(map);
 		if(totalCount < endRow) {
 			endRow = totalCount;				
