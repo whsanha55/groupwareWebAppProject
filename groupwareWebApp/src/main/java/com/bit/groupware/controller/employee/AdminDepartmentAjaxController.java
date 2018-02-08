0e27025d3bfb46634d302bb0c9d025acebfebf60
USER <USER@USER-PC>
2018-02-08 16:59:38 +0900

    asdf

--- groupwareWebApp/src/main/java/com/bit/groupware/controller/employee/AdminDepartmentAjaxController.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/controller/employee/AdminDepartmentAjaxController.java
@@ -2,6 +2,9 @@ package com.bit.groupware.controller.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bit.groupware.service.employee.EmployeeService;
 
@@ -11,8 +14,11 @@ public class AdminDepartmentAjaxController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	public void deptListAjaxController() {
+	@RequestMapping(value="/admin/deptListAjax.do", method=RequestMethod.POST)
+	public void deptListAjaxController(@RequestParam("oldHead")String oldHead,
+									   @RequestParam("newHead")String newHead) {
 		
+//		employeeService.up
 	}
 	
 }
