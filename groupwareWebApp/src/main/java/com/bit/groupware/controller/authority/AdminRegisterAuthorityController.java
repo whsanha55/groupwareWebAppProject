1db9f0ca9ecc5674797ac96437d2c0d3d503d65e
USER <USER@USER-PC>
2018-02-23 12:42:15 +0900

    ccccc

--- groupwareWebApp/src/main/java/com/bit/groupware/controller/authority/AdminRegisterAuthorityController.java
+++ groupwareWebApp/src/main/java/com/bit/groupware/controller/authority/AdminRegisterAuthorityController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bit.groupware.domain.authority.AuthorityVO;
@@ -29,10 +30,26 @@ public class AdminRegisterAuthorityController {
 	}
 
 	@RequestMapping(value="/admin/authority.do", method = RequestMethod.POST)
-	public String submit(AuthorityVO authority) {
-		logger.info("/////////////////authority///////////////////////", authority);
-		authorityService.registerAuthority(authority);
-		return "redirect:/admin/authorityList.do";
+	@ResponseBody
+	public Map<String, Object> submit(@RequestParam(value="aName") String aName, 
+												@RequestParam(value="aWhether") String aWhether,
+												@RequestParam(value="aNote") String aNote) {
+		
+		int name = authorityService.retrieveAuthorityByAname(aName); 
+		Map<String, Object> map = new HashMap<String, Object>();
+		AuthorityVO authority = new AuthorityVO();
+		authority.setaName(aName);
+		authority.setaWhether(aWhether);
+		authority.setaNote(aNote);
+		
+		if(name == 0) {
+			authorityService.registerAuthority(authority);
+			map.put("isSuccess", "true");
+		}else {
+			map.put("isSuccess", "false");
+		}
+		
+		return map;
 	}
 
 }
